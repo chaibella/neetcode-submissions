1a7420b31ec20ec1353495b8b1f9f2ffee869b32
chaibella <graceiscoding@gmail.com>
2026-07-17 10:42:14 -0700

    Add: combination-target-sum - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def dfs(i, path, remains):
+            if remains == 0: # added to target, add path to res
+                res.append(path[:])
+                return
+            if i == len(nums):
+                return # ran out of numbers
+
+            if nums[i] <= remains: # only use current num if it wont overflow
+                path.append(nums[i]) # use current num
+                dfs(i, path, remains - nums[i]) # and try
+                path.pop() # discard/skip current num
+                dfs(i + 1, path, remains) # and try with next num (larger than current)
+        
+        nums.sort() # sort to stop early
+        res = []
+        dfs(0, [], target)
+        return res
