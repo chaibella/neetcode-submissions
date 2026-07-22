697767d5ceb176f2ad121708596c9b882c267c97
chaibella <graceiscoding@gmail.com>
2026-07-21 17:19:22 -0700

    Add: search-for-word-ii - submission-14

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-14.py
@@ -0,0 +1,64 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = None
+
+
+class PrefixTree:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        cur = self.root
+        for ch in word:
+            if ch not in cur.children:
+                cur.children[ch] = TrieNode()
+            cur = cur.children[ch]
+        cur.word = word
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        trie = PrefixTree()
+        for word in words:
+            trie.insert(word)
+        root = trie.root
+
+        R, C = len(board), len(board[0])
+        res = []
+
+        def dfs(r, c, node):
+            if not (0 <= r < R and 0 <= c < C):
+                return # out of bounds
+            if board[r][c] not in node.children:
+                return # dead end
+            
+            ch = board[r][c]
+            nxt = node.children[ch]
+            if nxt.word:
+                res.append(nxt.word)
+                nxt.word = None
+            
+            board[r][c] = '#'
+            dfs(r, c - 1, nxt)
+            dfs(r, c + 1, nxt)
+            dfs(r - 1, c, nxt)
+            dfs(r + 1, c, nxt)
+            board[r][c] = ch
+
+        for r in range(R):
+            for c in range(C):
+                dfs(r, c, root)
+        
+        return res
+
+
+
+
+
+
+
+
+
+
+
+        
