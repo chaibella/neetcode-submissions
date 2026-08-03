7dbec79eedb6a276c8e27151d9eb33958869974e
chaibella <graceiscoding@gmail.com>
2026-08-03 16:48:47 -0700

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        def helper(houses):
+            prev2, prev1 = 0, 0
+            for house in houses:
+                prev2, prev1 = prev1, max(prev1, house + prev2)
+            return prev1
+
+        exclude_first = helper(nums[:len(nums) - 1])
+        exclude_last = helper(nums[1:])
+        return max(exclude_first, exclude_last)
